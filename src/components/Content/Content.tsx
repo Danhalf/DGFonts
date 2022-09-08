9d7d64054b5206737e00a2521e8bdd0537bfd505
Danhalf <danhel@rocketmail.com>
2022-09-08 12:43:22 +0300

    add debounce find

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,38 +1,51 @@
 const API_KEY = import.meta.env.VITE_API_KEY;
 import axios from 'axios';
-import { useState, useEffect } from 'react';
+import { FC, PropsWithChildren, useState, useEffect } from 'react';
+import { debounce } from '../../assets/helpers/helpers';
 import { IFont } from './ContentTypes';
 
 const url = `https://www.googleapis.com/webfonts/v1/webfonts?key=${API_KEY}`;
 
-const Content = () => {
+const Content: FC<PropsWithChildren> = ({ children }) => {
   const [fonts, setFonts] = useState([]);
+  const [currentFonts, setCurrentFonts] = useState(fonts);
 
   useEffect(() => {
     const fetchFonts = async () => {
       const result = await axios(url);
 
       const fonts = result.data.items;
-      // const fonts = Object.entries(result.data.items);
-      // console.log(result.data);
       setFonts(fonts);
+      setCurrentFonts(fonts);
     };
 
     fetchFonts();
   }, []);
 
+  const findFont = debounce(({ target }) => {
+    setCurrentFonts(
+      fonts.filter((font: IFont) =>
+        font.family.toLowerCase().includes(target.value.toLowerCase())
+      )
+    );
+  });
+
   return (
     <div className="min-h-screen bg-blue-500">
-      {fonts.map((font: IFont) => (
-        <ul key={font.family} className="mb-4">
+      <div>
+        <input type="text" onInput={findFont} />
+      </div>
+
+      {currentFonts.map((font: IFont) => (
+        <ul key={font.family as string} className="mb-4">
           <li>font-family: {font.family}</li>
           <li>
             font-files:
             {Object.entries(font.files).map((file) => {
               const [fontType, fontLink] = file;
               return (
-                <div key={fontLink}>
-                  type: {fontType}, <a href={fontLink}>download</a>
+                <div key={fontLink as string}>
+                  type: {fontType}, <a href={fontLink as string}>download</a>
                 </div>
               );
             })}
